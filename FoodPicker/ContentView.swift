192a915e7042197a4a88229230ec9dcb0532d839
dongganxixi <ruixing.xixi@gmail.com>
2024-07-29 19:22:49 +0800

    Initial Commit

---
+++ FoodPicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodPicker
+//
+//  Created by 胡瑞兴 on 2024/7/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
