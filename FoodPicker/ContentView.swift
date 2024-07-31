20fca2ccdda193e21e88620bedeb15317f9c96a6
dongganxixi <ruixing.xixi@gmail.com>
2024-07-31 15:04:02 +0800

    新增food picker 程序

--- FoodPicker/ContentView.swift
+++ FoodPicker/ContentView.swift
@@ -8,16 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    let food: [String] = ["饺子","汉堡","馄饨","包子","关东煮"]
+    @State var selectedFood: String?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        VStack(spacing:30) {
+            Image("dragon")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Text("今天吃什么？")
+                .bold()
+            
+            if selectedFood != .none {
+                Text(selectedFood ?? "")
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundStyle(.green)
+                
+                
+            }
+            
+            
+            /// 选择按钮
+            Button(action: {
+                selectedFood = food.shuffled().first{$0 != selectedFood}
+            }, label: {
+                Text(selectedFood == .none ? "告诉我" : "换一个")
+                    .frame(width: 200)
+                    .animation(.none, value: selectedFood) // selectedFood特定值更改时禁用动画
+                    .id(selectedFood)
+                    .transition(.asymmetric(
+                        insertion: .opacity
+                                   .animation(.easeInOut(duration: 0.5).delay(0.2)),
+                        removal: .opacity
+                                 .animation(.easeInOut(duration: 0.4))
+                    ))
+            })
+            .padding(.bottom, -15) // 将与下面的间距减少15
+            
+            /// 重置按钮
+            Button(action: {
+                selectedFood = .none
+            }, label: {
+                Text("重置").frame(width: 200)
+            })
+            .buttonStyle(.bordered)
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(content: {
+            Color(.secondarySystemBackground)
+        })
+        .ignoresSafeArea()
+        .font(.title)
+        .buttonStyle(.borderedProminent)
+        .buttonBorderShape(.capsule)
+        .controlSize(.large)
+        .animation(.easeInOut(duration: 0.6), value: selectedFood)
     }
+    
 }
+
+
+
 
 #Preview {
     ContentView()
