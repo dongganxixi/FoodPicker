f6857624892f3ac42b3c4a07e44513a08930219f
dongganxixi <ruixing.xixi@gmail.com>
2024-08-05 15:16:03 +0800

    完善代码view

--- FoodPicker/ContentView.swift
+++ FoodPicker/ContentView.swift
@@ -1,79 +1,151 @@
 //
 //  ContentView.swift
 //  FoodPicker
-//
-//  Created by 胡瑞兴 on 2024/7/29.
-//
+
 
 import SwiftUI
 
+
+
 struct ContentView: View {
-    let food: [String] = ["饺子","汉堡","馄饨","包子","关东煮"]
-    @State var selectedFood: String?
+    @State private var selectedFood: Food?
+    @State private var shouldShowInfo: Bool = false
+    
+    let food = Food.examples
     
     var body: some View {
-        
-        VStack(spacing:30) {
-            Image("dragon")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
+        VStack(spacing: 30) {
+            foodImage
             
-            Text("今天吃什么？")
-                .bold()
+            Text("今天吃什麼？").bold()
             
-            if selectedFood != .none {
-                Text(selectedFood ?? "")
-                    .font(.largeTitle)
-                    .bold()
-                    .foregroundStyle(.green)
-                
-                
-            }
+            selectedFoodInfoView
+             
+            Spacer().layoutPriority(1) // 按照它所在的view进行扩展画面，此例子是VStack，将沿着Y轴扩展
             
+            selectedButton
             
-            /// 选择按钮
-            Button(action: {
-                selectedFood = food.shuffled().first{$0 != selectedFood}
-            }, label: {
-                Text(selectedFood == .none ? "告诉我" : "换一个")
-                    .frame(width: 200)
-                    .animation(.none, value: selectedFood) // selectedFood特定值更改时禁用动画
-                    .id(selectedFood)
-                    .transition(.asymmetric(
-                        insertion: .opacity
-                                   .animation(.easeInOut(duration: 0.5).delay(0.2)),
-                        removal: .opacity
-                                 .animation(.easeInOut(duration: 0.4))
-                    ))
-            })
-            .padding(.bottom, -15) // 将与下面的间距减少15
-            
-            /// 重置按钮
-            Button(action: {
-                selectedFood = .none
-            }, label: {
-                Text("重置").frame(width: 200)
-            })
-            .buttonStyle(.bordered)
+            cancleButton
         }
         .padding()
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(content: {
-            Color(.secondarySystemBackground)
-        })
-        .ignoresSafeArea()
+        .frame(maxWidth:.infinity, maxHeight: .infinity)
+        .background(Color(.secondarySystemBackground))
         .font(.title)
         .buttonStyle(.borderedProminent)
         .buttonBorderShape(.capsule)
         .controlSize(.large)
         .animation(.easeInOut(duration: 0.6), value: selectedFood)
     }
+}
+
+// MARK: - subviews of the ContentView
+private extension ContentView {
     
+    /// 食物照片
+    var foodImage: some View{
+        // group{}是给里面的view添加共同的modifier用
+        Group{
+            if selectedFood != .none{
+                Text(selectedFood!.image)
+                    .font(.system(size: 200))
+                    .minimumScaleFactor(0.1)
+                    .lineLimit(1)
+            }else{
+                Image("dragon")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }}
+        .frame(height: 250)}
+    
+    var foodNameView: some View {
+        HStack {
+            Text(selectedFood!.name)
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(.green)
+                .id(selectedFood!.name)
+                .transition(.delayInsertionOpacity)
+            Button {
+                shouldShowInfo.toggle()
+            } label : {
+                Image(systemName: "info.circle.fill").foregroundColor(.secondary)
+            }.buttonStyle(.plain)
+        }
+    }
+    
+    var foodDetailView: some View {
+        VStack {
+            if shouldShowInfo {
+                Grid(horizontalSpacing: 12, verticalSpacing: 12) {
+                    GridRow {
+                        Text("蛋白質")
+                        Text("脂肪")
+                        Text("碳水")
+                    }.frame(minWidth: 60)
+                    
+                    Divider()
+                        .gridCellUnsizedAxes(.horizontal)
+                        .padding(.horizontal, -10)
+                    
+                    GridRow {
+                        Text(selectedFood!.$protein)
+                        Text(selectedFood!.$fat)
+                        Text(selectedFood!.$carb)
+                    }
+                }
+                .font(.title3)
+                .padding(.horizontal)
+                .padding()
+                .roundedRectBackground()
+                .transition(.moveUpWithOpacity)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .clipped()
+    }
+    
+    @ViewBuilder var selectedFoodInfoView: some View {
+        if selectedFood != .none {
+            foodNameView
+            
+            Text("熱量 \(selectedFood!.$calorie)").font(.title2)
+            
+            foodDetailView
+        }
+    }
+    
+    /// 选择按钮
+    var selectedButton: some View{
+        Button {
+            selectedFood = food.shuffled().first { $0 != selectedFood }
+        } label: {
+            Text(selectedFood == .none ? "告訴我" : "換一個").frame(width: 200)
+                .animation(.none, value: selectedFood)
+                .transformEffect(.identity)
+        }.padding(.bottom, -15)
+    }
+    
+    /// 取消按钮
+    var cancleButton: some View{
+        Button {
+            selectedFood = .none
+        } label: {
+            Text("重置").frame(width: 200)
+        }.buttonStyle(.bordered)
+    }
+}
+
+extension ContentView {
+    init(selectedFood: Food) {
+        _selectedFood = State(wrappedValue: selectedFood)
+    }
 }
 
 
-
-
-#Preview {
-    ContentView()
+/// preview
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
